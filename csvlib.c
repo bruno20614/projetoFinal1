c33eb7e4c86592f4624d363ff7a621dd41b9e67e
Alessandro <142166490+AlessandroVagnerIFCE@users.noreply.github.com>
2023-12-10 21:03:00 -0300

    Protótipo do algoritmo CSV

---
+++ csvlib.c
@@ -0,0 +1,22 @@
+#include "csvlib.h"
+
+void gerar_csv(unsigned char *m, unsigned int tam, char *filename, unsigned char rotulo)
+{
+    FILE *fp;
+    fp = fopen(filename, "w");
+    if (!fp) {
+        puts("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    //Gerar enumeração
+    for (unsigned int i = 1; i <= ((tam*tam)+1); i++) {
+        fprintf(fp, "%hu, ", i);
+    }
+    fprintf(fp, "\n");
+
+    for (unsigned int k = 0; k < (tam*tam); k++) {
+        fprintf(fp, "%hhu, ", *(m + k));
+    }
+    fprintf(fp, "%s\n", rotulo ? "stroma" : "epithelium");
+}
---
+++ csvlib.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+//Recebe uma matriz de unsigned char, o tamanho da matriz, o nome do arquivo e o rótulo (0 ou 1)
+//Gera um arquivo CSV com os dados
+void gerar_csv(unsigned char *, unsigned int, char *, unsigned char);
